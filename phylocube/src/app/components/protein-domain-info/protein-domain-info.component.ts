efb4f71802db44374aef409feafede40843b67d5
mikk <mikk.puustusmaa@gmail.com>
2018-06-26 15:26:43 +0300

    clanpfam resource has to use both api.

--- phylocube/src/app/components/protein-domain-info/protein-domain-info.component.ts
+++ phylocube/src/app/components/protein-domain-info/protein-domain-info.component.ts
@@ -9,7 +9,8 @@ import { CubeService } from '../../services/cube.service';
 })
 export class ProteinDomainInfoComponent implements OnInit {
 
-  selectedPoint=false;
+  selectedPoint;
+  activeResource;
 
   constructor(
     private resourceService: ResourceService, 
@@ -17,12 +18,35 @@ export class ProteinDomainInfoComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.resourceService.getActiveResource().subscribe(
+      resource => {
+        this.activeResource = resource;
+      },
+    );
+
     this.cubeService.getSelectedPoint().subscribe(
       point => {
         this.selectedPoint = point;
       }
 
     );
+  }
+
+  openNewTab($event,selectedPoint){
+    var acc = selectedPoint.acc
+    var api_url = this.activeResource.api_url;
+    if (this.activeResource.type == "clanpfam"){
+      // https://pfam.xfam.org/clan/;https://pfam.xfam.org/family/
+      var res = api_url.split(";");
+      if (acc.indexOf('CL') !== -1) {
+        api_url = res[0];
+      } else {
+        api_url = res[1];
+      }
+    } 
+    window.open(api_url + acc);
+    
+    
   }
 
 }
