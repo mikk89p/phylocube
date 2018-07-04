b84f545838d3d963935c866af8108f75e51f3fcb
mikk <mikk.puustusmaa@gmail.com>
2018-07-04 19:31:19 +0300

    Error dialog

---
+++ phylocube/src/app/components/error-dialog/error-dialog.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-error-dialog',
+  templateUrl: './error-dialog.component.html',
+  styleUrls: ['./error-dialog.component.scss']
+})
+export class ErrorDialogComponent implements OnInit {
+
+  title: string;
+  description: string;
+
+  constructor(
+    private dialogRef: MatDialogRef<ErrorDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) data) {
+    this.title = data.title;
+    this.description = data.description;
+}
+
+  ngOnInit() {
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+
+}
